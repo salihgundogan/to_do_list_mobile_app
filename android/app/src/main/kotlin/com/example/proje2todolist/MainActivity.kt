11cdb2bf0423d8a7e8c1d44eec94302acf758f58
salihgundogan <salih.gundogan@istun.edu.tr>
2024-12-26 19:59:56 +0300

    ilk yukleme

---
+++ android/app/src/main/kotlin/com/example/proje2todolist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proje2todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:proje2todolist/pages/home_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print("pausing...");
+    await Future.delayed(const Duration(seconds: 3));
+    print("unpausing...");
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
